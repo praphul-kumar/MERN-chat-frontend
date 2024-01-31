62aadaf5ebeaf63583344a8e9dee5667b47e4bb7
Praphul Kumar <praphulkumar2023@gmail.com>
2024-01-31 16:09:09 +0530

    Added : Basic Layout and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/chats' element={<Home />} />
+          {/* <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
