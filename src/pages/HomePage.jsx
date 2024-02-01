88512a37a4bc50ffa7b08e77e9a3675b17e806ef
Praphul Kumar <praphulkumar2023@gmail.com>
2024-02-01 13:05:52 +0530

    Added Previous UI Designs in latest Frontend module

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { Container } from '@chakra-ui/react'
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <Container maxW={'xl'} centerContent>
+      Home
+    </Container>
+  )
+}
